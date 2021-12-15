75350ae3078270e374416b7c181083b1f6c90c1c
carnoldcoding <carnold1@ithaca.edu>
2021-12-15 12:29:05 -0500

    Added commerce.js and Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product'
+import useStyles from './styles';
+
 
 const products = [
     {id: 1, name: "Shoes", description: "Running Shoes", price: '$5', image: 'https://res.cloudinary.com/atoms-shoes/image/upload/c_scale,w_1400,q_auto,f_auto/v1622733115/products/shoes/model000/black-white_profile'},
@@ -9,8 +11,10 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
     return (
-    <main>
+    <main className = {classes.content}>
+        <div className={classes.toolbar}/>
         <Grid container justify="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key = {product.id} xs={12} sm={6} md={4} lg={3}>
