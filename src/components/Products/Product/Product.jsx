d6570b0cab64b5bb8976e683d879fcd6bb284833
carnoldcoding <carnold1@ithaca.edu>
2021-12-15 16:17:14 -0500

    Functional cart back-end.

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -5,12 +5,13 @@ import React from 'react'
     import {AddShoppingCart, CallMissedSharp } from '@material-ui/icons';
     import useStyles from './styles';
 
-const Product = ({product}) => {
+const Product = ({ product, onAddToCart }) => {
     const classes = useStyles();
 
+    console.log(product);
     return (
         <Card className={classes.root}>
-            <CardMedia className={classes.media} image={product.image} title={product.name}/>
+            <CardMedia className={classes.media} image={product.image.url} title={product.name}/>
             <CardContent>
                 <div className={classes.cardContent}>
                     <Typography variant="h5" gutterBottom>
@@ -18,14 +19,14 @@ const Product = ({product}) => {
                     </Typography>
 
                     <Typography variant="h5">
-                        {product.price}
+                        {product.price.formatted_with_symbol}
                     </Typography>
                 </div>
-                <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary"/>
             </CardContent>
 
             <CardActions disableSpacing className={classes.cardActions}>
-                <IconButton aria-label="Add to Cart">
+                <IconButton aria-label="Add to Cart" onClick={() => onAddToCart(product.id, 1)}>
                     <AddShoppingCart/>
                 </IconButton>
             </CardActions>
