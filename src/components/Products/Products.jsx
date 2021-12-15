7e6d0661222f4592e5fd889a1d00998ad2d80b8a
carnoldcoding <carnold1@ithaca.edu>
2021-12-15 16:17:14 -0500

    Functional cart back-end.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,13 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product'
 import useStyles from './styles';
 
-
-const products = [
-    {id: 1, name: "Shoes", description: "Running Shoes", price: '$5', image: 'https://res.cloudinary.com/atoms-shoes/image/upload/c_scale,w_1400,q_auto,f_auto/v1622733115/products/shoes/model000/black-white_profile'},
-    {id: 2, name: "Macbook", description: "Apple Macbook", price: '$10', image: 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/mbp-spacegray-select-202011_GEO_US?wid=904&hei=840&fmt=jpeg&qlt=80&.v=1632948875000'},
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
     return (
     <main className = {classes.content}>
@@ -18,7 +12,7 @@ const Products = () => {
         <Grid container justifyContent="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key = {product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product}/>
+                    <Product product={product} onAddToCart={onAddToCart}/>
                 </Grid>
             ))}
         </Grid>
